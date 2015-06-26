224524c5ebb397f8633bc77a1ef0c8e5a3e66277
Kyle Wilson <kwilson@ooyala.com>
2015-06-26 13:24:58 -0700

    added run and stop scripts

--- run_collector.sh
+++ run_collector.sh
@@ -1,24 +1,24 @@
-KAKFKADIR="kafka_2.9.2-0.8.2.1/"
+KAFKADIR="kafka_2.9.2-0.8.2.1"
 SERVERCONFIG="config/server.properties"
 ZOOCONFIG="config/zookeeper.properties"
 
-if ["$1" = "-server-config"]; then
+if [ "$1" == "-server-config" ]; then
 	SERVERCONFIG=$2
 fi
 
-if ["$3" = "-server-config"]; then
+if [ "$3" == "-server-config" ]; then
 	SERVERCONFIG=$4
 fi
 
-if ["$1" = "-zoo-config"]; then
+if [ "$1" == "-zoo-config" ]; then
 	ZOOCONFIG=$2
 fi
 
-if ["$3" = "-zoo-config"]; then
+if [ "$3" == "-zoo-config" ]; then
 	ZOOCONFIG=$4
 fi
 
-cd $KAFKADIR	
-bin/zookeeper-server-start.sh config/zookeeper.properties &
-bin/kafka-server-start.sh config/server.properties &
+$KAFKADIR/bin/zookeeper-server-start.sh $KAFKADIR/$ZOOCONFIG &
+$KAFKADIR/bin/kafka-server-start.sh $KAFKADIR/$SERVERCONFIG & 
 go run http_server.go -brokers=":9092"
+
---
+++ stop_collector.sh
@@ -0,0 +1,5 @@
+KAFKADIR="kafka_2.9.2-0.8.2.1"
+
+$KAFKADIR/bin/zookeeper-server-stop.sh
+$KAFKADIR/bin/kafka-server-stop.sh 
+
